f0f289b585973d08c3ee779364a5abc64b81e507
cjkcg <161402871+cjkcg@users.noreply.github.com>
2024-11-22 14:00:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.cjkcg.us.kg" ]: dockerHub,
+  ["quay.cjkcg.us.kg" ]: "https://quay.io",
+  ["gcr.cjkcg.us.kg" ]: "https://gcr.io",
+  ["k8s-gcr.cjkcg.us.kg" ]: "https://k8s.gcr.io",
+  ["k8s.cjkcg.us.kg" ]: "https://registry.k8s.io",
+  ["ghcr.cjkcg.us.kg" ]: "https://ghcr.io",
+  ["cloudsmith.cjkcg.us.kg" ]: "https://docker.cloudsmith.io",
+  ["ecr.cjkcg.us.kg" ]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging.cjkcg.us.kg" ]: dockerHub,
 };
 
 function routeByHosts(host) {
